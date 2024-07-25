131390314c5c52b082016760da19e5c1b19a4c35
Nasim Alam <nasimal786@gmail.com>
2024-07-25 12:53:43 +0530

    add href

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -45,7 +45,7 @@ const ProductDetail = () => {
                     <StarRating rating={product.rating.rate} />
                   </span>
                   <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
-                    <a className="text-gray-500">
+                    <a href="https://www.google.co.uk/" className="text-gray-500">
                       <svg
                         fill="currentColor"
                         stroke-linecap="round"
@@ -57,7 +57,7 @@ const ProductDetail = () => {
                         <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                       </svg>
                     </a>
-                    <a className="text-gray-500">
+                    <a href="https://www.google.co.uk/" className="text-gray-500">
                       <svg
                         fill="currentColor"
                         stroke-linecap="round"
@@ -69,7 +69,7 @@ const ProductDetail = () => {
                         <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                       </svg>
                     </a>
-                    <a className="text-gray-500">
+                    <a href="https://www.google.co.uk/" className="text-gray-500">
                       <svg
                         fill="currentColor"
                         stroke-linecap="round"
